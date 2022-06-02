470113377369bc3b7bb7b7f0f15255009a284fda
Benthan Vu <bxvu@ucsc.edu>
2022-06-02 13:58:33 -0700

    finalized review

--- review/yelling.c
+++ review/yelling.c
@@ -6,22 +6,23 @@ int main(int argc, char **argv) {
   // read from stdin.
   // If you haven't seen them before, you will likely want to use the functions
   // `fgetc()` and `fputc()`.
-  FILE* infile;
+  FILE* infile = NULL;
   if (argc > 2) {
-    fprintf(stderr, "usage: ./yelling <file name>\n");
+    fprintf(stderr, "usage: ./yelling <file name> or cat <file name> | ./yelling\n");
     exit(1);
   }
   else if (argc == 2) {
     infile = fopen(argv[1], "r");
   }
-  int c = 0;
-  while ((c = getchar()) != EOF) {
+  int c = (infile) ? getc(infile) : getchar();
+  while (c != EOF) {
     if (c >= 'a' && c <= 'z'){
       putchar(c - ('a' - 'A'));
     }
     else {
       putchar(c);
     }
+    c = (infile) ? getc(infile) : getchar();
   }
   if (argc == 2) fclose(infile);
   return 0;
